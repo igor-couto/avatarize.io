2c9478532b7e7073aebd8d1eba36d61d312e6197
Igor Couto <igor.fcouto@gmail.com>
2021-04-21 23:27:27 -0300

    Add settings button action

--- src/app/settings-button/settings-button.component.ts
+++ src/app/settings-button/settings-button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings-button',
   templateUrl: './settings-button.component.html',
-  styleUrls: ['./settings-button.component.css']
+  styleUrls: ['./settings-button.component.css'],
 })
-export class SettingsButtonComponent implements OnInit {
+export class SettingsButtonComponent {
+  isPressed: boolean;
+  src: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.isPressed = false;
+    this.src = '../../assets/Images/settings-button-up.png';
   }
 
+  press() {
+    this.isPressed = true;
+    this.src = '../../assets/Images/settings-button-down.png';
+  }
+
+  release() {
+    this.isPressed = false;
+    this.src = '../../assets/Images/settings-button-up.png';
+  }
 }
